d553fc2ecd6f52e563487ff793b1492f33af6f14
ilfan <ilfanmuratov@gmail.com>
2021-10-15 22:21:20 +0500

    Courses new serializers

--- src/courses/serializers.py
+++ src/courses/serializers.py
@@ -1,8 +1,40 @@
 from rest_framework import serializers
-from .models import Course
+from .models import Course, Lesson, Exercise, Answer
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ['text', 'right']
+
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    answers = AnswerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Exercise
+        fields = ['title', 'exercise_type', 'text', 'answers']
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    exercises = ExerciseSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Lesson
+        fields = ['name', 'icon', 'cover', 'exercises']
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
+
     class Meta:
         model = Course
-        fields = ['id', 'name']
+        fields = [
+            'id',
+            'name',
+            'level',
+            'new_words',
+            'icon',
+            'cover',
+            'lessons'
+        ]
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,11 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import Course
 from .serializers import CourseSerializer
 
 
+class APICourseViewSet(ReadOnlyModelViewSet):
+    """Контроллер выдает список курсов, и отдельный курс по pk."""
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
 @api_view(['GET'])
-def api_all_courses(request):
+def courses(request):
     if request.method == 'GET':
         courses = Course.objects.all()
         serializer = CourseSerializer(courses, many=True)
