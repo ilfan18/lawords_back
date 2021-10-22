4b54c11ff6257e94b12417abae13da78e55ac961
ilfan <ilfanmuratov@gmail.com>
2021-10-22 18:20:33 +0500

    Spec urls change derectory

--- src/config/urls.py
+++ src/config/urls.py
@@ -11,11 +11,7 @@ urlpatterns = [
     ])),
     path('admin/', admin.site.urls),
     # * Документация
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/',
-         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/',
-         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', include('spectacular.urls')),
 ]
 
 if settings.DEBUG:
